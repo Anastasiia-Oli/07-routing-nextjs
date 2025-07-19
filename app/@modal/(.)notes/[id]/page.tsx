709e1ec8f42163fa24d746829eea90145016dc18
A_Oliinyk <aolejnik916@gmail.com>
2025-07-19 18:10:40 +0200

    fix errors 2.0

--- app/@modal/(.)notes/[id]/page.tsx
+++ app/@modal/(.)notes/[id]/page.tsx
@@ -1,11 +1,11 @@
 import { fetchNoteById } from "@/lib/api";
 import Modal from "@/components/Modal/Modal";
 import css from "./NotePreview.module.css";
-type Props = {
-  params: { id: string };
-};
+// type Props = {
+//   params: { id: string };
+// };
 
-const NotePreview = async ({ params }: Props) => {
+const NotePreview = async ({ params }: { params: { id: string } }) => {
   const { id } = params;
   const note = await fetchNoteById(id);
   return (
